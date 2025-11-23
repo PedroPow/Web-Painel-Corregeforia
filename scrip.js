778d56a3bff9b68affc78d1ee259f4b21c6d691b
Pedro Henrique Ferreira <pedrohfdias.15122007@gmail.com>
2025-11-23 03:47:34 -0300

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,32 @@
+const API_URL = "https://SEU_PROJETO_RENDER.onrender.com";
+
+async function atualizarDashboard() {
+    const res = await fetch(`${API_URL}/dashboard`);
+    const data = await res.json();
+
+    document.getElementById("convocacoes-count").innerText = data.convocacoes;
+    document.getElementById("pads-count").innerText = data.pads;
+    document.getElementById("ipms-count").innerText = data.ipms;
+}
+
+async function carregarConvocacoes() {
+    const res = await fetch(`${API_URL}/convocacoes`);
+    const lista = await res.json();
+
+    const tabela = document.getElementById("tabela-convocacoes");
+    tabela.innerHTML = "";
+
+    lista.forEach(item => {
+        tabela.innerHTML += `
+            <tr>
+                <td>${item.policial}</td>
+                <td>${item.data}</td>
+                <td>${item.hora}</td>
+                <td>${item.responsavel}</td>
+            </tr>
+        `;
+    });
+}
+
+setInterval(atualizarDashboard, 3000);
+setInterval(carregarConvocacoes, 3000);
